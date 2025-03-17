84b2f2b78c3e20c8d1e779154760c1866443c2f6
elasticspoon <mail@yuribocharov.dev>
2025-03-17 00:18:20 -0400

    fix: update for linux

--- db/setup_test_database.sh
+++ db/setup_test_database.sh
@@ -10,11 +10,11 @@ echo "Test DB"
 echo "%%%%%%%%%%%"
 
 # ROLES
-echo "SELECT 'CREATE USER $APP_TEST_USER WITH LOGIN' WHERE NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$APP_TEST_USER')\gexec" | psql $DB_URL
+echo "SELECT 'CREATE USER $APP_TEST_USER WITH LOGIN PASSWORD ''postgres''' WHERE NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$APP_TEST_USER')\gexec" | psql $DB_URL
 
 # DATABASE
 echo "Creating database $APP_TEST_DB_NAME"
-echo "SELECT 'CREATE DATABASE $APP_TEST_DB_NAME' WHERE NOT EXISTS (SELECT datname FROM pg_database WHERE datname = '$APP_TEST_DB_NAME')\gexec" | psql $DB_URL;
+echo "SELECT 'CREATE DATABASE $APP_TEST_DB_NAME' WHERE NOT EXISTS (SELECT datname FROM pg_database WHERE datname = '$APP_TEST_DB_NAME')\gexec" | psql $DB_URL
 psql $DB_URL -c "ALTER DATABASE $APP_TEST_DB_NAME OWNER TO $APP_TEST_USER"
 
 # SUPERUSER ONLY(!) for rideshare_test database test user
@@ -25,6 +25,6 @@ psql $DB_URL -c "ALTER USER $APP_TEST_USER WITH SUPERUSER"
 # CONNECT
 psql $DB_URL -c "GRANT CONNECT ON DATABASE $APP_TEST_DB_NAME TO $APP_TEST_USER;"
 
-psql -U $APP_TEST_USER -d $APP_TEST_DB_NAME -c "CREATE SCHEMA rideshare;"
-psql -U $APP_TEST_USER -d $APP_TEST_DB_NAME -c "ALTER ROLE $APP_TEST_USER SET search_path TO rideshare;"
-psql -U $APP_TEST_USER -d $APP_TEST_DB_NAME -c "SET search_path TO rideshare;"
+psql -U $APP_TEST_USER -h localhost -d $APP_TEST_DB_NAME -c "CREATE SCHEMA rideshare;"
+psql -U $APP_TEST_USER -h localhost -d $APP_TEST_DB_NAME -c "ALTER ROLE $APP_TEST_USER SET search_path TO rideshare;"
+psql -U $APP_TEST_USER -h localhost -d $APP_TEST_DB_NAME -c "SET search_path TO rideshare;"
